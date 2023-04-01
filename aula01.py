4e6d77a82cfaa3ab280354ac05cce0b1cf2de31c
wolfblack27 <tigowolfblack27@gmail.com>
2023-04-01 20:13:10 -0300

    'logicos'

--- aula01.py
+++ aula01.py
@@ -1,25 +1,42 @@
 n1 = int(input("Digite uma numero maior que 0: \n"))
-n2 = int(input("Digite uma numero maior que 0: \n"))
 
+
+if (n1>0):
+    
+    if(n1%2!=0) and (n1//n1==1):
+        print(f'O numero {n1} é primo' )
+    
+    
+else:
+    print('nao faça')
+
+
+
+ 
+
+"""""
+contador=0
+vn1=0
+n1 = int(input("Digite uma numero maior que 0: \n"))
+n2 = int(input("Digite uma numero maior que 0: \n"))
 
 
 if (n1>=0) and (n1<n2):
 
     print(f'Numeros pares de: {n1} até {n2}' )
-   
+    vn1=n1
     while n1<=n2:
 
         if(n1%2==0):
             print(n1)
+            contador+=1
 
         n1+=1
     
 
-
-
 else:
     print('nao faça')
 
+print(f'Quantidade de numeros pares de {vn1} até {n2}: {contador}')
 
-#print(f'numero (1)foi: {n1}  numero (2): {n2}')
-
+ """
