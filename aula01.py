5b3dda8e3073ac0acec0c5a27ee14fc6d55c2e03
wolfblack27 <tigowolfblack27@gmail.com>
2023-04-01 19:30:03 -0300

    'updata'

--- aula01.py
+++ aula01.py
@@ -1,2 +1,25 @@
-msg = "isso é somente um teste"
-print(msg)
+n1 = int(input("Digite uma numero maior que 0: \n"))
+n2 = int(input("Digite uma numero maior que 0: \n"))
+
+
+
+if (n1>=0) and (n1<n2):
+
+    print(f'Numeros pares de: {n1} até {n2}' )
+   
+    while n1<=n2:
+
+        if(n1%2==0):
+            print(n1)
+
+        n1+=1
+    
+
+
+
+else:
+    print('nao faça')
+
+
+#print(f'numero (1)foi: {n1}  numero (2): {n2}')
+
