c07635c51b82b57f8c282e7b704d81ca648800ca
wolfblack27 <tigowolfblack27@gmail.com>
2023-04-06 11:12:00 -0300

    classes_Poli_Her

--- aula10.py
+++ aula10.py
@@ -8,11 +8,39 @@ class Passaro:
 
 
     def atacar(self, alvo):
-        pass
+        print("Metodo classe mae")
+        print(f'Executando {alvo}')
+        print("Metodo de classe mae realizado")
+        return 'Fim do metodo'
 
     def fugir(self, destino):
         pass
     
 class PassaroAerreo(Passaro):
-        
-    pass
+    def __init__(self, vida, ataque, defesa,companheiro):
+        self.companheiro = companheiro
+        super().__init__(vida, ataque, defesa)
+    
+    def atacar(self, alvo):
+        print("Metodo sobrecarga")
+        print(f'Executando {alvo}')
+        print("Metodo de sobrecarga realizado")
+        return 'Fim do metodo'
+
+
+class PassaroAquatico(Passaro):
+    def __init__(self, vida, ataque, defesa,forcanado):
+        self.forcanado = forcanado
+        super().__init__(vida, ataque, defesa)
+    
+    def atacar(self, alvo):
+        return super().atacar(alvo)
+
+
+#Teste Classe:
+
+aguia = PassaroAerreo(500,700,800,'thiago')
+pinguin = PassaroAquatico(700,700,500,1000)
+
+aguia.atacar('cobra')
+pinguin.atacar('peixe')
