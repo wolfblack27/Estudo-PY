8ca70ebdcb4215a0372f40b98d1154a5c50b354a
wolfblack27 <tigowolfblack27@gmail.com>
2023-04-06 22:19:55 -0300

    req_online

--- requisicao_dados.py
+++ requisicao_dados.py
@@ -3,7 +3,7 @@ import requests
 "https://economia.awesomeapi.com.br/json/daily/BTC-BRL/1"
 "https://economia.awesomeapi.com.br/json/daily/USD-BRL/1"
 
-listaMoedas = ['BTC-USD','BTC-BRL','USD-BRL']
+listaMoedas = ['BTC-USD','BTC-BRL','USD-BRL','BNB-USD']
 moeda = int(input ("Qual moeda deseja informações: (0-BTC-USD) (1-BTC-BRL)"))
 qtd = int(input("Dugute a quantidade de dias: "))
 requisicao = requests.get(f'https://economia.awesomeapi.com.br/json/daily/{listaMoedas[moeda]}/{qtd}')
